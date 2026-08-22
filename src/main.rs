913e63fa6d6245aea4b96840857cd624a58086f6
ashfract <callum.bit@proton.me>
2026-08-22 17:10:12 +0100

    feat: define CLI args (width, height, target)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::path::PathBuf;
+
+/// Lightweight image to ASCII art conversion tool
+#[derive(Parser, Debug)]
+#[command(name = "ascii_art_generator", version, about)]
+struct Args {
+    #[arg(short = 'W', long)]
+    width: u32,
+    #[arg(short = 'H', long)]
+    height: u32,
+    target: PathBuf,
 }
+
+fn main() {}
