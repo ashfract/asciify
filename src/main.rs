2550542228ec40e04be09222574d5bdaf0e8e87d
ashfract <callum.bit@proton.me>
2026-08-27 22:05:03 +0100

    feat: function to calculate aspect ratio from width and height

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,11 @@ fn fetch_image(args: Args) -> Result<image::GrayImage, Box<dyn std::error::Error
     Ok(gray)
 }
 
+fn calculate_aspect_ratio(width: u32, height: u32) -> f32 {
+    let ratio = height as f32 / width as f32;
+    ratio
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
